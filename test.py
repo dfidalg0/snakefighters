459e54ff43b2c2baf29d1be60e1ab815fe1e2c70
Diego <diego-fidalgo@hotmail.com>
2020-05-18 20:44:23 -0300

    Adicionado teste das classes básicas

---
+++ test.py
@@ -0,0 +1,44 @@
+from game import pg, Screen, GameObject
+from time import sleep
+
+def get_close():
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            return True
+    else:
+        return False
+
+
+screen = Screen()
+
+SNAKE_FIG = pg.image.load('assets/img/snake_test.png')
+
+master = GameObject(screen, SNAKE_FIG, y=180, vy=-5)
+slv1 = GameObject(master, SNAKE_FIG, -200,100)
+slv2 = GameObject(slv1, SNAKE_FIG, -200,100)
+
+for i in range(100):
+    if get_close():
+        exit()
+    screen.update()
+    sleep(0.001)
+    master.update()
+    slv1.update()
+    slv2.update()
+
+master.set_spd((4,0))
+slv1.set_spd((-4,0))
+slv2.set_spd((5,5))
+
+for i in range(100):
+    if get_close():
+        break
+    screen.update()
+    sleep(0.001)
+    master.update()
+    slv1.update()
+    slv2.update()
+
+while True:
+    if get_close():
+        break

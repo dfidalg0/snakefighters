f72608d0492272c81effd20234399d662316cd16
Diego <diego-fidalgo@hotmail.com>
2020-05-20 15:38:04 -0300

    Adicionada mecânica de atualização do Player

--- game/player.py
+++ game/player.py
@@ -12,44 +12,43 @@ class Player:
         body = GameObject(screen, imgb, x - 100, y)
         tail = GameObject(screen, imgt, x - 200, y)
         self.__nodes = [head, body, tail]
+        self.__grow = False
 
     def get_nodes(self):
         return self.__nodes
 
     def grow_size(self, x, y):
-        self.__nodes[0].set_img(self.__imgbody)
-        node = GameObject(self.__screen, self.__imghead, x, y)
-        node.set_spd(self.__nodes[0].get_spd())
-        self.__nodes = [node] + self.__nodes
+        self.__grow = True
+
+    def update (self):
+        if self.__grow:
+            self.__grow = False
+
+            new_spd = self.__nodes[0].get_spd()
+            new_pos = self.__nodes[0].get_pos()
+            new_node = GameObject(self.__screen, self.__imgbody, *new_pos, *new_spd)
+
+            self.__nodes.insert(1,new_node)
+        else:
+            for i in range(len(self.__nodes__)-1, 0, -1):
+                self.__nodes[i].update()
+                new_spd = self.__nodes[i-1].get_spd()
+                self.__nodes[i].set_spd(new_spd)
+
+        self.__nodes[0].update()
 
     def up(self):
         if self.__nodes[0].get_spd() != Vector2(0,1):
-            speed = Vector2(0, -1)
-            for node in self.__nodes:
-                speedaux = node.get_spd()
-                node.set_spd(speed)
-                speed = speedaux
+            self.__nodes[0].set_spd((0,-1))
 
     def down(self):
         if self.__nodes[0].get_spd() != Vector2(0, -1):
-            speed = Vector2(0, 1)
-            for node in self.__nodes:
-                speedaux = node.get_spd()
-                node.set_spd(speed)
-                speed = speedaux
+            self.__nodes[0].set_spd((0,1))
 
     def left(self):
         if self.__nodes[0].get_spd() != Vector2(1, 0):
-            speed = Vector2(-1, 0)
-            for node in self.__nodes:
-                speedaux = node.get_spd()
-                node.set_spd(speed)
-                speed = speedaux
+            self.__nodes[0].set_spd((-1,0))
 
     def right(self):
         if self.__nodes[0].get_spd() != Vector2(-1, 0):
-            speed = Vector2(1, 0)
-            for node in self.__nodes:
-                speedaux = node.get_spd()
-                node.set_spd(speed)
-                speed = speedaux
+            self.__nodes[0].set_spd((1,0))

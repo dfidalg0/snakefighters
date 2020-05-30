570c13e49b8e34bc7af0cd541d4858258fe453a6
Diego <diego-fidalgo@hotmail.com>
2020-05-30 18:01:46 -0300

    Alterações leves no funcionamento da classe Screen

--- game/screen.py
+++ game/screen.py
@@ -8,9 +8,10 @@ class Screen:
         self.__screen = pg.display.set_mode(resolution)
         self.__pos = Vector2(resolution[0] / 2, resolution[1] / 2)
         self.__color = color
-        self.__resolution = resolution
+        self.__resolution = Vector2(resolution)
         self.__slaves = deque()
-        self.__background = pg.image.load('assets/img/background.jpg')
+        background = pg.image.load('assets/img/background.jpg')
+        self.__background = pg.transform.scale(background,resolution)
 
     def update(self):
         self.__screen.blit(self.__background, (0, 0))

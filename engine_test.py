ea7e4fa272fcb2f8598662f02b82b97f524303a4
pedro <freitassilvap@gmail.com>
2020-06-07 17:53:21 -0300

    Ajustes de mapa

--- engine_test.py
+++ engine_test.py
@@ -9,7 +9,7 @@ game = GameEngine(screen, arena)
 
 WALL = pg.transform.scale(pg.image.load('assets/img/parede.png'), (gunity, gunity))
 
-game.load_map(maps['cross_and_borders'])
+game.load_map(maps['eights'])
 
 for x in range(-30 * gunity, +30 * gunity+1, gunity):
     InterfaceObject(arena, WALL,x,+15*gunity)
@@ -19,7 +19,7 @@ for y in range(-14 * gunity, +14 * gunity+1, gunity):
     InterfaceObject(arena, WALL,+30*gunity,y)
     InterfaceObject(arena, WALL,-30*gunity,y)
 
-game.add_player(imgsety, 1, -200, 100, [pg.K_UP, pg.K_LEFT, pg.K_DOWN, pg.K_RIGHT])
-game.add_player(imgsetb, -1, 200, 100, [pg.K_w, pg.K_a, pg.K_s, pg.K_d])
+game.add_player(imgsety, 1, -200, 0, [pg.K_UP, pg.K_LEFT, pg.K_DOWN, pg.K_RIGHT])
+game.add_player(imgsetb, -1, 200, 0, [pg.K_w, pg.K_a, pg.K_s, pg.K_d])
 
 game.game_loop()
